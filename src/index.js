4016942cd4cf3e9b400bd29a4416d6433a0da277
Frotrest <125584696+frotrest@users.noreply.github.com>
2026-04-30 12:06:53 +0300

    Perfomance was provided

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 const playBtn = document.getElementById('play-btn');
 const ipDisplay = document.getElementById('ip-display');
 
+document.addEventListener('DOMContentLoaded', () => {
+  try {
+    const animatedElements = document.querySelectorAll('[data-animate]');
+
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting && entry.target instanceof Element) {
+            const animation = entry.target.dataset.animate;
+            entry.target.classList.add(
+              'animate__animated',
+              `animate__${animation}`
+            );
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        root: null,
+        threshold: 0.1,
+      }
+    );
+
+    animatedElements.forEach(el => {
+      observer.observe(el);
+    });
+  } catch (error) {
+    console.log(`Ошибка с анимациями: ${error}`);
+  }
+});
+
 playBtn.addEventListener('click', function () {
   navigator.clipboard
     .writeText('mc.novatime.pp.ua')
